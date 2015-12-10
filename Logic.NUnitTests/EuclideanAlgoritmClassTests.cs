7e802bec517a84b95ae80d7215c492052a2ca692
Islam-Mamayeu <Islam_Mamayeu@epam.com>
2015-12-10 18:42:05 +0300

    First Commit

---
+++ Logic.NUnitTests/EuclideanAlgoritmClassTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using NUnit.Framework;
+
+
+namespace Logic.NUnitTests
+{
+    [TestFixture]
+    public class EuclideanAlgoritmClassTests
+    {
+        [TestCase(1000,575,Result = 25)]
+
+        public int EuclideanAlgoritmTest(int a,int b)
+        {
+            Stopwatch timer = new Stopwatch();
+            int actResult = EuclideanAlgoritmClass.Algoritm(timer,a,b);
+            return actResult;
+        }
+
+        [TestCase(1000, 575,746, Result = 1)]
+
+        public int EuclideanAlgoritmTest(int a, int b,int c)
+        {
+
+            int actResult = EuclideanAlgoritmClass.Algoritm(EuclideanAlgoritmClass.Algoritm(a, b),c);
+            return actResult;
+        }
+
+
+    }
+}
---
+++ Logic/EuclideanAlgoritmClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Logic
+{
+    public class EuclideanAlgoritmClass
+    {
+        public static int  Algoritm(Stopwatch timer, int a, int b)
+        {
+            timer.Start();
+            int t;
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while (b != 0)
+            {
+                t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+        public static int Algoritm(Stopwatch timer, int a, int b, int c)
+        {
+            return Algoritm(timer, Algoritm(timer, a, b), c);
+        }
+
+        public static int Algoritm(Stopwatch timer, params int[] arg)
+        {
+            int temp = Algoritm(timer, arg[0], arg[1]);
+
+            for (int i = 2; i < arg.Length; i++)
+            {
+                temp = Algoritm(timer, temp, arg[i]);
+            }
+
+            return temp;
+
+        }
+
+
+
+
+    }
+}
